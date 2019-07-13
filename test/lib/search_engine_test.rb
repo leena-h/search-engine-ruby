a152d7f11848439a45841d026df619996a2679d4
Leena <leena@hardhat.com.au>
2019-07-13 20:28:02 +1000

    Add comment to search engine test

--- test/lib/search_engine_test.rb
+++ test/lib/search_engine_test.rb
@@ -43,6 +43,8 @@ class SearchEngineTest < Minitest::Test
     expected = user_with_related_records()
     result = @instance.perform_search(indice_type: 'users', term: '_id', value: 1)
     result_keys = result.map {|hit| hit.keys}.flatten.uniq
+
+    # Format results to have array of ids to easily compare objects
     result.each do |user|
       user['assigned_ticket_ids'] = user['assigned_tickets'].map { |at| at['_id'] }
       user['submitted_ticket_ids'] = user['submitted_tickets'].map { |st| st['_id'] }
@@ -64,25 +66,30 @@ class SearchEngineTest < Minitest::Test
 
   def user_without_related_records
     {
-      "_id"=>1,
-      "url"=>"http://initech.zendesk.com/api/v2/users/1.json",
-      "external_id"=>"74341f74-9c79-49d5-9611-87ef9b6eb75f",
-      "name"=>"Francisca Rasmussen",
-      "alias"=>"Miss Coffey",
-      "created_at"=>"2016-04-15T05:19:46 -10:00",
-      "active"=>true,
-      "verified"=>true,
-      "shared"=>false,
-      "locale"=>"en-AU",
-      "timezone"=>"Sri Lanka",
-      "last_login_at"=>"2013-08-04T01:03:27 -10:00",
-      "email"=>"coffeyrasmussen@flotonic.com",
-      "phone"=>"8335-422-718",
-      "signature"=>"Don't Worry Be Happy!",
-      "organization_id"=>119,
-      "tags"=>["Springville", "Sutton", "Hartsville/Hartley", "Diaperville"],
-      "suspended"=>true,
-      "role"=>"admin"
+      "_id"=> 1,
+      "url" => "http://initech.zendesk.com/api/v2/users/1.json",
+      "external_id" => "74341f74-9c79-49d5-9611-87ef9b6eb75f",
+      "name" => "Francisca Rasmussen",
+      "alias" => "Miss Coffey",
+      "created_at" => "2016-04-15T05:19:46 -10:00",
+      "active"=> true,
+      "verified"=> true,
+      "shared"=> false,
+      "locale" => "en-AU",
+      "timezone" => "Sri Lanka",
+      "last_login_at" => "2013-08-04T01:03:27 -10:00",
+      "email" => "coffeyrasmussen@flotonic.com",
+      "phone" => "8335-422-718",
+      "signature" => "Don't Worry Be Happy!",
+      "organization_id"=> 119,
+      "tags" => [
+        "Springville",
+        "Sutton",
+        "Hartsville/Hartley",
+        "Diaperville"
+      ],
+      "suspended"=> true,
+      "role" => "admin"
     }
   end
 
